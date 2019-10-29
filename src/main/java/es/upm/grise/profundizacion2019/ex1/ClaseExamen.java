66a895b279b96a1eed31c3ce48101eed27a21daf
Fernando Alonso <1falonso97@gmail.com>
2019-10-29 16:00:02 +0100

    Ejercicio terminado

--- src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
@@ -6,14 +6,19 @@ public class ClaseExamen {
 		
 		float result = 0;
 		
+		// nodo 1
 		while( a > b) {
 			a--;
 		}
 		
+		// nodo 2
 		if (a <= b) {
+			// nodo 3
 			result = 0;
 		}
+		
 		else {
+			// nodo 4
 			result = b;
 		}
 		
--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -2,13 +2,41 @@ package es.upm.grise.profundizacion2019.ex1;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * 
+ * @author Fernando Alonso Pastor
+ *
+ */
 public class ClaseExamenTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	private ClaseExamen cl;
+	
+	// Los nombres de los métodos, indican el camino, el último camino, no se puede probar
+	
+	
+	@Before
+	public void Initialize()
+	{
+		cl = new ClaseExamen();
 	}
 
+	@Test
+	public void test_1_2_3_5() {
+		assertEquals(cl.metodoAProbar(10, 10), 0, 0.001);
+	}
+	
+	@Test
+	public void test_1_1_2_3_5() {
+		assertEquals(cl.metodoAProbar(11, 10), 0, 0.001);
+	}
+	
+	@Test
+	public void test_1_2_4_5() {
+		// No se puede probar esta rama, no es posible acceder a ella, ya que:
+		// a siempre será menor o igual que b al llegar a esa parte
+	}
+	
 }
